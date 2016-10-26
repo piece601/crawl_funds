45f8598de74217dcb39fb2656a49bbe349689369
Piece Chao <piece601@hotmail.com>
2016-10-26 19:45:14 +0800

    first commit

--- class/db.php
+++ class/db.php
@@ -1,3 +1,3 @@
 <?php
 $dsn = "mysql:host=localhost;dbname=fund_db";
-$db = new PDO($dsn, 'root', 'coppqyt', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$db = new PDO($dsn, 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
